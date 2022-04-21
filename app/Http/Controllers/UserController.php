840d6b3500b7e60971e9257d19886db7df44ebc7
NorayrKroyan <98810255+NorayrKroyan@users.noreply.github.com>
2022-04-21 16:10:01 +0400

    Request

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,18 +16,9 @@ use Illuminate\Validation\Rules\Exists;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     public function index(Request $request)
     {
-        if($request->ajax()) {
-            // ajax request
-            return response()->json(['status' => 'success']);
-        }
-
         $data = User::orderBy('id','DESC')->paginate(5);
        
         return view('users.index',compact('data'))
@@ -36,23 +27,14 @@ class UserController extends Controller
         ->with('k', ($request->input('page', 1) - 1) * 5);
     }
     
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     public function create()
     {
         $roles = Role::pluck('name','name')->all();
         return view('users.create',compact('roles'));
     }
     
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    
     public function store(StoreUserRequest $request)
     {
         
@@ -81,24 +63,14 @@ class UserController extends Controller
                         ->with('success','User created successfully');
     }
     
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+  
     public function show($id)
     {
         $user = User::find($id);
         return view('users.show',compact('user'));
     }
     
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+   
     public function edit($id)
     {
         $user = User::find($id);
@@ -108,13 +80,7 @@ class UserController extends Controller
         return view('users.edit',compact('user','roles','userRole'));
     }
     
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function update(UpdateEditRequest $request, $id)
     {
         
@@ -166,12 +132,7 @@ class UserController extends Controller
                         ->with('success','User updated successfully');
     }
     
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+   
     public function destroy($id)
     {   
         
